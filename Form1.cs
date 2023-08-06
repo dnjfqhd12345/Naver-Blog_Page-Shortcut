2220f091d3951ec3f6087f53b4892c61c5a7883d
unknown <dnjfqhd12345@gmail.com>
2023-08-07 00:36:28 +0900

    naver-blog_page-shortcut_v.1.0

---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked == false && radioButton1.Checked == false)
+            {
+                MessageBox.Show("검색 단위를 먼저 선택해야 합니다!", "경고", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                String url = "https://blog.naver.com/PostList.naver?from=postList&blogId=";
+                url += textBox1.Text;
+                url += "&categoryNo=0&currentPage=";
+                int num;
+                if (radioButton2.Checked)
+                {
+                    if(textBox1.Text == "" || textBox2.Text == "")
+                    {
+                        MessageBox.Show("방문하실 블로그 아이디와 탐색할 글번호 및 페이지 번호를 입력해주세요.", "경고", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+                    try
+                    {
+                        num = 5 * Int32.Parse(textBox2.Text);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("글번호나 페이지 번호는 숫자를 입력하셔야만 합니다.", "경고", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+                    num = 5 * Int32.Parse(textBox2.Text);
+                    url += num.ToString();
+                }
+                else
+                {
+                    if (textBox1.Text == "" || textBox2.Text == "")
+                    {
+                        MessageBox.Show("방문하실 블로그 아이디와 탐색할 글번호 및 페이지 번호를 입력해주세요.", "경고", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+                    try
+                    {
+                        num = 5 * Int32.Parse(textBox2.Text);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("글번호나 페이지 번호는 숫자를 입력하셔야만 합니다.", "경고", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+
+                    url += textBox2.Text;
+                }
+                System.Diagnostics.Process.Start(url);
+            }
+
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("제작자: 전산쟁이(zzzz7037)\n블로그에 방문하시면 업데이트 소식을 받아보실 수 있습니다.","프로그램 제작자 정보",MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://blog.naver.com/zzzz7037");
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Form2.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(262, 353);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(283, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "닫기";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(776, 335);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(800, 412);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form2";
+            this.Text = "사용 방법";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
